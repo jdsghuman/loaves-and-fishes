f4faa9bcdd3d241d2423fa987e69aff727500f2d
Ryan Mundy <ryanmundy34@gmail.com>
2019-02-04 16:23:44 -0600

    adds onsite home view, needs functionality

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,6 +3,7 @@ import errors from './errorsReducer';
 import loginMode from './loginModeReducer';
 import user from './userReducer';
 import userListReducer from './userListReducer';
+import locationReducer from './locationReducer';
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
 // This is imported in index.js as rootSaga
@@ -14,6 +15,7 @@ const rootReducer = combineReducers({
   loginMode, // will have a value of 'login' or 'registration' to control which screen is shown
   user, // will have an id and username if someone is logged in
   userListReducer,
+  locationReducer,
 });
 
 export default rootReducer;
