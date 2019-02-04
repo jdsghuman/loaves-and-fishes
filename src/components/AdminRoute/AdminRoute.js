4a6784c22be3edbcd6129cf51e5c9a304f4dfe63
Abdul Ismail <Abdulrismail9@gmail.com>
2019-02-04 10:23:55 -0600

    created admin view and route

---
+++ src/components/AdminRoute/AdminRoute.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {Route} from 'react-router-dom'
+import {connect} from 'react-redux';
+import LoginPage from '../LoginPage/LoginPage';
+import RegisterPage from '../RegisterPage/RegisterPage';
+
+// A Custom Wrapper Component -- This will keep our code DRY.
+// Responsible for watching redux state, and returning an appropriate component
+// API for this component is the same as a regular route
+
+// THIS IS NOT SECURITY! That must be done on the server
+// A malicious user could change the code and see any view
+// so your server-side route must implement real security
+// by checking req.isAuthenticated for authentication
+// and by checking req.user for authorization
+
+const AdminRoute = (props) => {
+  // Using destructuring, this takes ComponentToProtect from component
+  // prop and grabs all other props to pass them along to Route
+  const {
+    // Alias prop 'component' as 'ComponentToProtect'
+    component: ComponentToProtect,
+    user,
+    loginMode,
+    ...otherProps
+  } = props;
+
+  let ComponentToShow;
+
+  if(user.admin) {
+    // if the user is logged in (only logged in users have ids)
+    // show the component that is protected
+    ComponentToShow = ComponentToProtect;
+  } else if (user.admin === false) {
+    // if they are not logged in, check the loginMode on Redux State
+    // if the mode is 'login', show the LoginPage
+    ComponentToShow = LoginPage;
+  } else {
+    // the the user is not logged in and the mode is not 'login'
+    // show the RegisterPage
+    ComponentToShow = RegisterPage;
+  }
+
+  // We return a Route component that gets added to our list of routes
+  return (
+      <Route
+        // all props like 'exact' and 'path' that were passed in
+        // are now passed along to the 'Route' Component
+        {...otherProps}
+        component={ComponentToShow}
+      />
+  )
+}
+
+// Instead of taking everything from state, we just want the user and loginMode
+// to determine which page we should show the user
+// if you wanted you could write this code like this:
+// const mapStateToProps = ({ user, loginMode }) => ({ user, loginMode });
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+    loginMode: state.loginMode,
+    admin: state.admin,
+  }
+}
+
+export default connect(mapStateToProps)(AdminRoute)
+
+
