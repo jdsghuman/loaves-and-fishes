3dca4118a668854932cb3c26aff80f69d78077ba
Victoria Chhieng <victoria_chhieng@hotmail.com>
2019-02-04 14:19:21 -0600

    fix error

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,7 +16,7 @@ import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
 import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
 import InfoPage from '../InfoPage/InfoPage';
-import AdminHome from '../Admin/AdminHome';
+import AdminHome from '../Admin/AdminHome/AdminHome';
 import AdminRoute from '../AdminRoute/AdminRoute';
 
 
