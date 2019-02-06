8cba71525b2517f68d0b8dcc32022c06f6f449fa
Ryan Mundy <ryanmundy34@gmail.com>
2019-02-06 16:04:33 -0600

    updates users last location

--- src/redux/sagas/locationSaga.js
+++ src/redux/sagas/locationSaga.js
@@ -8,7 +8,7 @@ function* fetchLocations() {
             headers: { 'Content-Type': 'application/json' },
             withCredentials: true,
         };
-        
+
         const response = yield axios.get('/api/location', config);
         yield put({ type: 'SET_LOCATIONS', payload: response.data });
     } catch (error) {
@@ -16,8 +16,17 @@ function* fetchLocations() {
     }
 }
 
+function* addLastLocation(action) {
+    try {
+        yield axios.put('/api/user/location', action.payload);
+    } catch (error) {
+        console.log('User get request failed', error);
+    }
+}
+
 function* locationSaga() {
     yield takeEvery('FETCH_LOCATIONS', fetchLocations);
+    yield takeEvery('SET_LAST_LOCATION', addLastLocation);
 }
 
 export default locationSaga;
