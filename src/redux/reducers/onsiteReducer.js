cb8f830b6d8e72d7874ccdcbbcac4d452119a302
JD Ghuman <jdsghuman@gmail.com>
2019-02-07 10:34:49 -0600

    Fix refresh error on meal count screens

--- src/redux/reducers/onsiteReducer.js
+++ src/redux/reducers/onsiteReducer.js
@@ -1,4 +1,27 @@
-const onSiteReducer = (state = {}, action) => {
+const onSite = {
+    "value":'',
+    "selectedLocation":{  
+       "id": '',
+       "location_name": '',
+       "street_address": '',
+       "city": '',
+       "state": '',
+       "zip": '',
+       "county": '',
+       "active": '',
+       "notes": '',
+       "updated_by": '',
+       "date_updated": ''
+    },
+    "collectDemographics": '',
+    "farm": '',
+    "summer": '',
+    "time": '',
+    "userId": '',
+    "lastLocationId": ''
+ }
+
+const onSiteReducer = (state = onSite, action) => {
     switch (action.type) {
         case 'SET_ONSITE':
             return action.payload;
