ffab038e61c79ba3f805176e973eeaf3c44bc3ff
Ryan Mundy <ryanmundy34@gmail.com>
2019-02-13 10:15:40 -0600

    removes inactive locations from onsite home

--- src/redux/sagas/locationSaga.js
+++ src/redux/sagas/locationSaga.js
@@ -16,6 +16,20 @@ function* fetchLocations() {
     }
 }
 
+function* fetchActiveLocations() {
+    try {
+        const config = {
+            headers: { 'Content-Type': 'application/json' },
+            withCredentials: true,
+        };
+
+        const response = yield axios.get('/api/location/active', config);
+        yield put({ type: 'SET_LOCATIONS', payload: response.data });
+    } catch (error) {
+        console.log('User get request failed', error);
+    }
+}
+
 function* addLastLocation(action) {
     try {
         yield axios.put('/api/user/location', action.payload);
@@ -27,6 +41,7 @@ function* addLastLocation(action) {
 function* locationSaga() {
     yield takeEvery('FETCH_LOCATIONS', fetchLocations);
     yield takeEvery('SET_LAST_LOCATION', addLastLocation);
+    yield takeEvery('FETCH_ACTIVE_LOCATIONS', fetchActiveLocations);
 }
 
 export default locationSaga;
