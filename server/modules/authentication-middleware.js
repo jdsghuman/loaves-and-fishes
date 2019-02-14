5c53a04442e1aed287159c2b5862455c42aa2d7a
Ryan Mundy <ryanmundy34@gmail.com>
2019-02-14 10:54:53 -0600

    updates alerts

--- server/modules/authentication-middleware.js
+++ server/modules/authentication-middleware.js
@@ -4,8 +4,10 @@ const rejectUnauthenticated = (req, res, next) => {
     // They were authenticated! User may do the next thing
     // Note! They may not be Authorized to do all things
     next();
-  } else {
+  }
+  else {
     // failure best handled on the server. do redirect here.
+
     res.sendStatus(403);
   }
 };
