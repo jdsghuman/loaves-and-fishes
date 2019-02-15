15293d6d54f1c4a5288bf572930eae90eb1d24b5
JD Ghuman <jdsghuman@gmail.com>
2019-02-14 20:45:16 -0600

    Add sweet alert to the home page. Edit the footer text

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -20,9 +20,6 @@ const Nav = (props) => (
         {/* Show the link to the info page and the logout button if the user is logged in */}
         {props.user.id && (
           <>
-            {/* <Link className="nav-link" to="/info">
-            Info Page
-          </Link> */}
             {props.user.admin &&
               <Link className="nav-link" to="/admin">
                 Admin
